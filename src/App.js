f8a1ebc846d1d027186a867dc885e95a85104797
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-03-16 13:36:23 -0400

    added imgs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { Link, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import react from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Montpelier Zombie Guide</h1>
       </header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>All Guides</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      <div>
+        <p>
+          Welcome to Montpelier Zombie Guide. Here you will find everything you
+          need to know in order to survive a zombie apocalypse. There are guides
+          for just about everything. Feel free to look around and if you have
+          any suggestions, please leave a comment. Guides are adjustable and
+          user input is encouraged. Stay safe....
+        </p>
+        <img
+          src="./Users/justinkay/GA/Topaz/unit_2/week_3/monday/mzg/src/photos/main.jpeg"
+          height="200px"
+          width="200px"
+          alt="Main Street"
+        />
+      </div>
     </div>
   );
 }
