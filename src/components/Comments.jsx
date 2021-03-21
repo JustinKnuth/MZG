39ab25bb335e9db864e5a39ed1b71a3ed08d2d57
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-03-21 15:35:31 -0400

    styling form

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -12,8 +12,8 @@ console.log(props)
   return (
     <div className= "comments">
       <h3>{props.comment.fields.content}</h3>
-      <h4>{props.comment.fields.name}</h4>
-      <h4>{props.comment.fields.date}</h4>
+      <h4><em>{props.comment.fields.name}</em></h4>
+      <h5>{props.comment.fields.date}</h5>
     </div>
 
   );
